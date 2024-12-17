cf928d4511167e0502a19c5617e0d26d3d10ff34
Revprm <revy.pramana@gmail.com>
2024-12-17 07:33:35 +0700

    feat: Add Flask app with prediction endpoint and requirements file

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 from flask import Flask, request, jsonify
+import joblib
+
+app = Flask(__name__)
+
+model = joblib.load('models/XGBmodel.pkl')
+scaler = joblib.load('models/scaler.pkl')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+
+    prediction = model.predict([list(data.values())])
+
+    output = prediction[0]
+    return jsonify(output)
